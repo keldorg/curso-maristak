bf6a82a72dfcba0f3e69ae7c9359c287f8f137a4
Keldor <keldor.gerrika@gmail.com>
2018-10-23 09:07:12 +0200

    FizzBuzz Kata

--- Modulo-3/FizzBuzz_Kata/src/fizzbuzz.js
+++ Modulo-3/FizzBuzz_Kata/src/fizzbuzz.js
@@ -1,15 +1,16 @@
 class Fizzbuzz {
     constructor(){
         this.numeros = []
+        let factoria = new FactoriaNumeros()
 
         for (let i=1; i <= 100; i++) {
-            this.numeros.push(i)
+            this.numeros.push(factoria.crearNumero(i))
         }
     }
 
     run() {
         for (let numero of this.numeros) {
-            console.log(numero)
+            console.log(numero.getNumero())
         }
     }
 
