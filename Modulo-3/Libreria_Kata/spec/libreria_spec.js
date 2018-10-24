9cf4621154274e5e21fdf1a2a0deb119c6d9c98c
Keldor <keldor.gerrika@gmail.com>
2018-10-24 10:03:35 +0200

    Añadida Kata Librería

---
+++ Modulo-3/Libreria_Kata/spec/libreria_spec.js
@@ -0,0 +1,11 @@
+describe("Librería", function() {
+
+    describe("Añadir un cliente a la librería", function () {
+        const libreria = new Libreria();
+
+      it("Debería de añadir un cliente", function () {
+
+      })
+    })
+
+});
